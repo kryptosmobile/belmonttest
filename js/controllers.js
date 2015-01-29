eecd52d12bd6315c18c9777319e48b93dcf2f095
AK <ak@kryptosmobile.com>
2015-01-30 00:23:14 +0530

    Bug fixes and metadata update

--- js/controllers.js
+++ js/controllers.js
@@ -75,15 +75,6 @@ angular.module('myApp.controllers', []).
     }])
     .controller('LoginCtrl', ['$scope', '$rootScope', '$http', '$location',
         function ($scope, $rootScope, $http, $location) {
-			setTimeout(function () {
-				$('#loginfrm').on('submit', function(event) {
-						event.preventDefault();
-						$rootScope.login();
-						$("#loginbtn").focus();
-						return false;
-    			});
-			},2000);
-
             $rootScope.back = function () {
                 $location.path("/home");
             };
